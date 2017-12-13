f56d2d749172dd547d74781982323893164428e2
Vitaljok <vitalijs.komasilovs@gmail.com>
2017-12-13 13:23:41 +0200

    Migrating application to Spring container

--- web-api/src/main/java/lv/llu/itf/demo/charging/Application.java
+++ web-api/src/main/java/lv/llu/itf/demo/charging/Application.java
@@ -1,15 +1,28 @@
 package lv.llu.itf.demo.charging;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import java.math.BigDecimal;
 
-public class Application {
-    public static void main(String[] args) {
-        new Application().run();
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    public Application(PaymentService service) {
+        this.service = service;
     }
 
-    public void run() {
-        PaymentService service = new PaymentService();
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
 
+    private PaymentService service;
+
+    @Override
+    public void run(String... args) {
         BigDecimal totalAmount = new BigDecimal("300.00");
         int numOfPayments = 5;
         BigDecimal paymentAmount = service.getPaymentAmount(totalAmount, numOfPayments);
--- web-api/src/main/java/lv/llu/itf/demo/charging/PaymentService.java
+++ web-api/src/main/java/lv/llu/itf/demo/charging/PaymentService.java
@@ -1,9 +1,12 @@
 package lv.llu.itf.demo.charging;
 
+import org.springframework.stereotype.Service;
+
 import java.math.BigDecimal;
 
 import static java.math.BigDecimal.ROUND_HALF_EVEN;
 
+@Service
 public class PaymentService {
     public BigDecimal getPaymentAmount(BigDecimal totalAmount, Integer numOfPayments) {
         return totalAmount.divide(new BigDecimal(numOfPayments), 2, ROUND_HALF_EVEN);
