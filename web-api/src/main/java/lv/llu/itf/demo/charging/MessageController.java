c6b6eee7858070bb2e33a2ee2f020676e9090a24
Vitaljok <vitalijs.komasilovs@gmail.com>
2018-02-19 13:10:14 +0200

    Configuring authorization

--- web-api/src/main/java/lv/llu/itf/demo/charging/MessageController.java
+++ web-api/src/main/java/lv/llu/itf/demo/charging/MessageController.java
@@ -1,9 +1,8 @@
 package lv.llu.itf.demo.charging;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/messages")
@@ -19,8 +18,19 @@ public class MessageController {
         return "User message: Welcome, " + auth.getName();
     }
 
+    @GetMapping("/user/{id}")
+    @PreAuthorize("#id != 100")
+    public String getUserMessageDetails(@PathVariable Long id) {
+        return "User detailed message: " + id;
+    }
+
     @GetMapping("/admin")
     public String getAdminMessage(Authentication auth) {
         return "Admin message: Hi, " + auth.getName();
+    }
+
+    @PostMapping
+    public String changeMessages(Authentication auth) {
+        return "Changed by  " + auth.getName();
     }
 }
--- web-api/src/main/java/lv/llu/itf/demo/charging/SecurityConfig.java
+++ web-api/src/main/java/lv/llu/itf/demo/charging/SecurityConfig.java
@@ -3,9 +3,26 @@ package lv.llu.itf.demo.charging;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfig {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .mvcMatchers("/messages/public").permitAll()
+                .mvcMatchers("/messages/user/*").hasRole("USER")
+                .mvcMatchers("/messages/admin").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll();
+    }
+
 
     @Autowired
     public void configureUsers(AuthenticationManagerBuilder auth) throws Exception {
